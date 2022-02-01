909f9334899432fe0cf23814ff2711d16890ed17
hayoung <proshy1@naver.com>
2022-02-01 19:37:06 +0900

    wip: mono-repo, rollup config

---
+++ readme.md
@@ -0,0 +1,4 @@
+## rollup config reference
+
+- [blog-1](https://ideveloper2.dev/blog/2020-05-17--rollup-ts-%EB%A1%9C-%EB%94%94%EC%9E%90%EC%9D%B8%EC%8B%9C%EC%8A%A4%ED%85%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0/)
+- [blog-2](https://so-so.dev/pattern/mono-repo-config/)
---
+++ rollup.config.js
@@ -0,0 +1,59 @@
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import typescript from "rollup-plugin-typescript2";
+
+const excludePath = "node_modules/**";
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+
+export default {
+  input: "src/main.ts",
+  output: {
+    file: "bundle.js",
+    format: "cjs",
+  },
+  plugins: [typescript()],
+};
+
+function buildJS(input, output, format) {
+  const defaultOutputConfig = {
+    format,
+    exports: "named",
+    sourcemap: true,
+  };
+
+  const esOutputConfig = {
+    ...defaultOutputConfig,
+    dir: output,
+  };
+  const cjsOutputConfig = {
+    ...defaultOutputConfig,
+    file: output,
+  };
+
+  const config = {
+    input,
+    external: ["react"],
+    output: [format === "es" ? esOutputConfig : cjsOutputConfig],
+    plugins: [
+      peerDepsExternal(),
+      commonjs(),
+      resolve({
+        preferBuiltins: false,
+        extensions,
+      }),
+      babel({
+        presets: ["@babel/env", "@babel/preset-typescript"],
+        plugins: ["@babel/plugin-transform-runtime"],
+        babelHelpers: "runtime",
+        exclude: excludePath,
+        extensions,
+      }),
+    ],
+    preserveModules: format === "es",
+  };
+
+  return config;
+}
