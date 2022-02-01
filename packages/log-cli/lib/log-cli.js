d33f5816386463b230712883184a3408b545dfc5
hayoung <proshy1@naver.com>
2022-02-01 14:13:08 +0900

    clear all

--- packages/log-cli/lib/log-cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { program } from 'commander';
-import LogCore from 'log-core-test-kyle';
-
-// action
-program.action((cmd) => LogCore());
-
-program.parse(process.argv);
--- packages/log-core/lib/log-core.js
+++
@@ -1,5 +0,0 @@
-import red from 'chalk';
-
-export default function core() {
-  console.log(red('❤  Running Core !!!!!'));
-}
